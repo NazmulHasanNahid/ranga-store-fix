6f87ff7cbd5d38cdb613dbb351b6182cd93787ab
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2021-09-13 02:38:14 +0600

    total and rating fixed

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <p>Rating : ${product.rating.rate}</p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -30,7 +31,7 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
+  updateTotal()
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
@@ -76,5 +77,8 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+    document.getElementById("total").innerText = grandTotal ;
+
 };
+
+
