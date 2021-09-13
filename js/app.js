a4c1efd647173786d59521839cbc02d5ea8ec07f
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2021-09-13 10:15:04 +0600

    grand total fixed

--- js/app.js
+++ js/app.js
@@ -48,12 +48,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = parseFloat(total).toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = parseFloat(value).toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -74,12 +74,9 @@ const updateTaxAndCharge = () => {
 };
 
 //grandTotal update function
-const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-    document.getElementById("total").innerText = grandTotal ;
-
+const updateTotal = () =>{
+  const grandTotal = parseFloat(document.getElementById("price").innerText)+ parseFloat( document.getElementById("delivery-charge").innerText) + parseFloat( document.getElementById("total-tax").innerText); 
+  document.getElementById("total").innerText = parseFloat (grandTotal).toFixed(2);
 };
 
 
